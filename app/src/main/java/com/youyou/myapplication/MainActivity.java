955baee583387f253ab8ab958fea3b27b83e4e36
youyouing <yuemeiling@163.com>
2017-11-02 17:37:19 +0800

    自定义控件

--- app/src/main/java/com/youyou/myapplication/MainActivity.java
+++ app/src/main/java/com/youyou/myapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.youyou.myapplication;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import view.CustomTitleView;
 
 /**
  * 程序入口
@@ -16,27 +24,35 @@ import android.widget.Toast;
  */
 
 public class MainActivity extends AppCompatActivity {
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Toast.makeText(MainActivity.this, "接受到消息了。。。。。", Toast.LENGTH_SHORT).show();
-        }
-    };
+    @Bind(R.id.custom)
+    CustomTitleView custom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String s = null;
-        if (!"hello".equals(s)) {
-            Log.d("yml", "程序顺利执行");
-        }
-        new Thread(new Runnable() {
+        ButterKnife.bind(this);
+        custom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                handler.sendEmptyMessage(0);
+            public void onClick(View view) {
+                custom.setmTitleText(randomText());
             }
-        }).start();
+        });
+
+    }
+
+    private String randomText() {
+        Random random = new Random();
+        Set<Integer> set = new HashSet<Integer>();
+        while (set.size() < 4) {
+            int randomInt = random.nextInt(10);
+            set.add(randomInt);
+        }
+        StringBuffer sb = new StringBuffer();
+        for (Integer i : set) {
+            sb.append("" + i);
+        }
+
+        return sb.toString();
     }
 }
